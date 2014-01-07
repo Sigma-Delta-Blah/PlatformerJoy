4de393dcd24b14d401459f913ccd2cf4bae55438
princek14 <visually.basic.keith@gmail.com>
2014-01-07 13:17:07 -0500

    working

--- PlatformerJoy/PlatformerMyScene.m
+++ PlatformerJoy/PlatformerMyScene.m
@@ -20,8 +20,11 @@
         
         myLabel.text = @"Welcome!";
         myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        myLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        
+        _bob = [[TileMap alloc] init];
+        
+        [_bob setMapofWidth: 14 andHeight: 20];
         
         [self addChild:myLabel];
     }
@@ -41,8 +44,22 @@
         SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
         
         [sprite runAction:[SKAction repeatActionForever:action]];
+<<<<<<< HEAD
         
         [self addChild:sprite];
+=======
+
+        for (int number = 0; number < 14; number++) {
+            for (int number2 = 0; number2 < 20; number2++) {
+                if([_bob getTileAtX:number andY:number2]) {
+                    NSLog([NSString stringWithFormat:@"Phillium %d, %d is good to go!", number, number2]);
+                } else {
+                    NSLog([NSString stringWithFormat:@"Phillium %d, %d is having problems!  WE ARE GOING DOWN!!!  AAAAARRRRGGGGHHHHNJ#KRWJEGFNTBHGTKLRWFMFNTOKJENVG!!!!!!!!!!!!!!!!!!!!!", number, number2]);
+                }
+                [self addChild:[_bob getTileAtX:number andY:number2]];
+            }
+        }
+>>>>>>> fed57cf121b613abd97b51b30f7222bb8bec9bec
     }
 }
 
