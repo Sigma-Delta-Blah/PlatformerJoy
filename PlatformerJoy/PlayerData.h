96d4d2af5b3a8ad194026edb3369e4ef8d94eb4a
jackalopeking <jackelopeking@yahoo.com>
2014-03-04 12:58:28 -0500

    BLAHHHHHHHH!!!!!!!!!  RONALD MCDONALD TOUHOOOOOOOOOOOOUUUUUUUUUSSSSSSSSS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ PlatformerJoy/PlayerData.h
@@ -0,0 +1,30 @@
+//
+//  PlayerData.h
+//  PlatformerJoy
+//
+//  Created by Joseph Mooney on 2/28/14.
+//  Copyright (c) 2014 WOE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+#import "PhysicsController.h"
+
+@interface PlayerData : NSObject
+
+@property (strong, nonatomic)SKSpriteNode *objSprite;
+@property (strong, nonatomic)PhysicsController * physics;
+@property (strong, nonatomic)UITouch *setTouch;
+@property (nonatomic) float groundYPos;
+@property (nonatomic)BOOL jumping;
+@property (nonatomic)BOOL moving;
+
+-(void) createPlayer: (SKNode *) world;
+
+-(void) movementPlayerBegin: (NSSet *) touches inScene: (SKNode *) scene;
+
+-(void) movementPlayerEnd: (NSSet *) touches inScene: (SKNode *) scene;
+
+-(void) movementPlayerUpdate: (SKNode *) scene;
+
+@end
